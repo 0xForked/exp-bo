17d36cc981b656dfd7f31acaadbd8b48a442a4e1
A. A. Sumitro <hello@aasumitro.id>
2022-02-14 05:20:39 +0800

    WIP

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import { createHead } from "@vueuse/head";
 
 import "@/assets/index.css";
+import "@/assets/main.css";
 
 import App from "@/App.vue";
 import router from "@/router";
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,10 +7,8 @@
     <div class="relative flex">
         <div class="w-full md:w-1/2">
             <div class="min-h-screen h-full flex flex-col after:flex-1">
-                    <!-- Header -->
                 <div class="flex-1">
                     <div class="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
-                        <!-- Logo -->
                         <a class="block" href="#">
                             <svg width="32" height="32" viewBox="0 0 32 32">
                                 <defs>
@@ -33,51 +31,40 @@
                 </div>
     
                 <div class="max-w-sm mx-auto px-4 py-8">
-        
-                    <h1 class="text-3xl text-gray-800 font-bold mb-6">Welcome back! ✨</h1>
-                        <!-- Form -->
+                    <h1 class="text-3xl text-gray-800 font-bold mb-6">
+                        <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">Welcome back!</span> 
+                        ✨
+                    </h1>
                     <form>
                         <div class="space-y-4">
                             <div>
-                                <label class="block text-sm font-medium mb-1" for="email">Email Address</label>
-                                <input id="email" class="form-input w-full" type="email" />
+                                <label class="block text-sm font-medium mb-1" for="username">Username</label>
+                                <input id="text" class="form-input w-full" type="username" />
                             </div>
                             <div>
                                 <label class="block text-sm font-medium mb-1" for="password">Password</label>
                                 <input id="password" class="form-input w-full" type="password" autocomplete="on" />
                             </div>
+                            <div class="flex items-center justify-between mt-6">
+                                <div class="mr-1"></div>
+                                <a class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-3" href="index.html">Sign In</a>
+                            </div>
                         </div>
                     </form>
-
-                        <!-- Footer -->
-                        <div class="pt-5 mt-6 border-t border-gray-200">
-                            <div class="text-sm">
-                                Don’t you have an account? <a class="font-medium text-indigo-500 hover:text-indigo-600" href="signup.html">Sign Up</a>
-                            </div>
-                            <!-- Warning -->
-                            <div class="mt-5">
-                                <div class="bg-yellow-100 text-yellow-600 px-3 py-2 rounded">
-                                    <svg class="inline w-3 h-3 shrink-0 fill-current" viewBox="0 0 12 12">
-                                        <path d="M10.28 1.28L3.989 7.575 1.695 5.28A1 1 0 00.28 6.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 1.28z" />
-                                    </svg>
-                                    <span class="text-sm">
-                                        To support you during the pandemic super pro features are free until March 31st.
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-        
-                    </div>
-
                 </div>
-
             </div>
+        </div>
 
             <!-- Image -->
         <div class="hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2" aria-hidden="true">
-            <img class="object-cover object-center w-full h-full" src="@/assets/img/auth-background.jpg" width="760" height="1024" alt="Authentication image" />
+            <img class="object-cover object-center w-full h-full" src="https://images.unsplash.com/photo-1640622842936-b81247a2d4a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" width="760" height="1024" alt="Authentication image" />
             <img class="absolute top-1/4 left-0 transform -translate-x-1/2 ml-8 hidden lg:block" src="@/assets/img/auth-decoration.png" width="218" height="224" alt="Authentication decoration" />
         </div>
     </div>
   </main>
-</template>H
+</template>
+
+<style>
+
+
+</style>
